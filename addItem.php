11d14bf82b7b07ca390b9cdfc0349eadf6852fa9
Chien-Chi Liu <68536635+JessicaLiuC@users.noreply.github.com>
2021-12-11 22:39:20 -0500

    Item CRUD done

--- addItem.php
+++ addItem.php
@@ -1,1 +1,52 @@
-<!-- addItem -->
+<?php
+
+ini_set("display_errors", 1);
+ini_set("display_start_errors", 1);
+error_reporting(E_ALL);
+
+$username = "yvh5398";
+$password = 'a245125022';
+$host = 'localhost';
+$dbname = 'yvh5398_431W';
+
+$fname = $_POST['sellerID'];
+$lname = $_POST['itemID'];
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Simple E-Commerce Website</title>
+    </head>
+    <body>
+        <p>
+            <?php
+                echo "Inserting new seller item: " . $SID . " " . $itemID . "...";
+                $sql = "INSERT INTO Item (sellerID, itemID) VALUES ("$sellerID", "$itemID")";
+                try {
+                    $conn = new PDO("mysql:host = $host; dbname = $dbname", $username, $password);
+                    $conn ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                    $conn ->exec($sql);
+                    echo "New record created successfully"; 
+            ?>
+                <p>You will be redirected in 3 seconds</p>
+                <script>
+                    var timer = setTimeout(function(){
+                        window.location="start.php"
+                    }, 3000);
+                </script>
+            <?php
+                }
+                catch(PDOException $e){
+                    echo $sql . "<br>" . $e->getMessage();
+                }
+                $conn = null;
+            ?>
+        </p>
+    </body>
+</div>
+</html>
+
+
+
+
