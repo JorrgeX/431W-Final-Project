3c8b5c5a2b9e9a5e92e17020ab7b027d6f959a09
Chien-Chi Liu <68536635+JessicaLiuC@users.noreply.github.com>
2021-12-11 23:55:12 -0500

    Item CRUD done

--- updateItem.php
+++ updateItem.php
@@ -1,1 +1,50 @@
-<!-- updateItem -->
+<!--updateItem-->
+
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$username = 'yvh5398';
+$password = 'a245125022';
+$host = 'localhost';
+$dbname = 'yvh5398_431W';
+
+$sellerID = $_POST["sellerID"];
+$itemID = $_POST['itemID'];
+
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Simple E-Commerce Website</title>
+    </head>
+    <body>
+		<p>
+			<?php 
+				echo "Updating item: " . $itemID . "..."; 
+				$sql = "UPDATE SellerItem SET sellerID='$sellerID', itemID='$itemID' WHERE sellerID='$sellerID' ";
+				try {
+					$conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+					$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+					$conn->exec($sql);
+					echo "Item updated successfully";
+				?>
+				<p>You will be redirected in 3 seconds</p>
+				<script>
+					var timer = setTimeout(function() {
+						window.location='start.php'
+					}, 3000);
+				</script>
+			<?php
+				} catch(PDOException $e) {
+					echo $sql . "<br>" . $e->getMessage();
+				}
+				$conn = null;
+			?>
+		</p>
+    </body>
+</div>
+</html>
